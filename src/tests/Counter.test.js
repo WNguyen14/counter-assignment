abeb752876fa4d4f5d40026fcf6229663318a5d1
WNguyen14 <wnguyen1@ufl.edu>
2023-11-19 20:09:25 -0500

    unit test changes

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,36 @@
 // import necessary react testing library helpers here
+import { fireEvent, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 // import the Counter component here
-
+import Counter from "../components/Counter";
 beforeEach(() => {
-  // Render the Counter component here
-})
-
-test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  render(<Counter></Counter>);
 });
 
-test('should render initial count with value of 0', () => {
+//expect a counter message
+test("renders counter message", () => {
   // Complete the unit test below based on the objective in the line above
+  expect(screen.getByText(/Counter/i)).toBeInTheDocument();
 });
 
-test('clicking + increments the count', () => {
+//expect the message to be 0
+test("should render initial count with value of 0", () => {
   // Complete the unit test below based on the objective in the line above
+  expect(screen.getByText(/0/i)).toBeInTheDocument();
 });
 
-test('clicking - decrements the count', () => {
+//expect the text to be 1 since we only pushed the button once
+test("clicking + increments the count", () => {
   // Complete the unit test below based on the objective in the line above
+  const button = screen.getByText("+");
+  fireEvent.click(button);
+  expect(screen.getByText(/1/i)).toBeInTheDocument();
+});
+
+//expect the text to be -1 since we decremented once
+test("clicking - decrements the count", () => {
+  // Complete the unit test below based on the objective in the line above
+  const button = screen.getByText("-");
+  fireEvent.click(button);
+  expect(screen.getByText(/-1/i)).toBeInTheDocument();
 });
